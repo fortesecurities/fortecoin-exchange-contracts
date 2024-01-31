b41bfc713b643e81bd44a98ada14f2083947bae7
Quentin Adam <quentinadam@gmail.com>
2024-01-31 17:24:54 +0100

    WIP

--- Exchange.sol
+++ Exchange.sol
@@ -1,26 +1,10 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
 import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
-import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
-
+import {IERC20Permit} from "@openzeppelin/contracts/token/ERC20/extensions/IERC20Permit.sol";
+import {ExtendedAccessControlUpgradeable} from "./ExtendedAccessControlUpgradeable.sol";
 import {LinkedListLibrary, LinkedList} from "./LinkedList.sol";
-
-using LinkedListLibrary for LinkedList;
-
-struct Request {
-    address account;
-    uint128 id;
-    uint32 price;
-    int64 amount;
-    uint32 deadline;
-}
-
-struct Limiter {
-    uint256 interval;
-    uint256 limit;
-}
-
-//TODO: custom events
+import {Limiter, LimiterLibrary} from "./Limiter.sol";
 
 function floorDiv(int256 a, int256 b) pure returns (int256 quotient) {
     quotient = a / b;
@@ -30,70 +14,161 @@ function floorDiv(int256 a, int256 b) pure returns (int256 quotient) {
     }
 }
 
-contract Exchange is Ownable {
-    IERC20 public immutable base;
-    IERC20 public immutable quote;
-    address public immutable vault;
-    uint256 public constant decimals = 6;
-    uint64 miniumumAmount = 1000000000;
+contract Exchange is ExtendedAccessControlUpgradeable {
+    struct Request {
+        address account;
+        uint128 id;
+        uint32 price;
+        int64 amount;
+        uint32 deadline;
+    }
 
+    error SenderUnauthorized();
+    error DeadlineExpired();
+    error PriceTooLow();
+    error PriceTooHigh();
+    error AmountTooLow();
+    error LimitExceeded();
+
+    bytes32 public constant LIMIT_ROLE = keccak256("LIMIT_ROLE");
+    bytes32 public constant ACCEPT_ROLE = keccak256("ACCEPT_ROLE");
+
+    address public immutable BASE_TOKEN;
+    address public immutable QUOTE_TOKEN;
+    address public immutable VAULT;
+    uint256 public constant PRICE_DECIMALS = 6;
+    uint64 public constant MINIMUM_AMOUNT = 1000000000;
+
+    Limiter limiter;
+    using LimiterLibrary for Limiter;
     LinkedList requestIds;
+    using LinkedListLibrary for LinkedList;
     mapping(uint128 => Request) requests;
 
-    constructor(address owner, IERC20 _base, IERC20 _quote, address _inventory) Ownable(owner) {
-        base = _base;
-        quote = _quote;
-        vault = _inventory;
+    /**
+     * @dev Emitted when a trade is accepted.
+     */
+    event AcceptTrade(address indexed account, uint128 indexed id, uint32 price);
+
+    /**
+     * @dev Emitted when the 24-hour transfer limit is changed to `limit`.
+     */
+    event ChangeLimit(uint256 limit);
+
+    /**
+     * @dev Emitted when a request is cancelled.
+     */
+    event DeleteRequest(address indexed account, uint128 indexed id);
+
+    /**
+     * @dev Emitted when a trade is requested.
+     */
+    event RequestTrade(address indexed account, uint128 indexed id, uint32 price, int64 amount, uint32 deadline);
+
+    /**
+     * @dev Emitted when the 24-hour transfer limit is temporarily decreased by `limitDecrease`.
+     */
+    event TemporarilyDecreaseLimit(uint256 limitDecrease);
+
+    /**
+     * @dev Emitted when the 24-hour transfer limit is temporarily increased by `limitIncrease`.
+     */
+    event TemporarilyIncreaseLimit(uint256 limitIncrease);
+
+    constructor(address _base, address _quote, address _inventory) {
+        _disableInitializers();
+        BASE_TOKEN = _base;
+        QUOTE_TOKEN = _quote;
+        VAULT = _inventory;
     }
 
-    function requestTrade(uint32 _price, int64 _amount, uint32 _deadline) public {
-        requestTrade(msg.sender, _price, _amount, _deadline);
+    function initialize(address defaultAdmin) public initializer {
+        __ExtendedAccessControl_init();
+        _addRole(LIMIT_ROLE);
+        _addRole(ACCEPT_ROLE);
+        _grantRoles(defaultAdmin);
+        limiter.interval = 24 hours;
     }
 
-    function requestTrade(address _account, uint32 _price, int64 _amount, uint32 _deadline) public {
-        uint128 id = requestIds.generate();
-        if (_amount > 0) {
-            require(uint64(_amount) >= miniumumAmount, "Amount too low");
-        } else {
-            require(uint64(-_amount) >= miniumumAmount, "Amount too low");
+    function _deleteRequest(uint128 id) internal {
+        requestIds.remove(id);
+        delete requests[id];
+        emit DeleteRequest(msg.sender, id);
+    }
+
+    function acceptTrade(uint128 requestId, uint32 price) public onlyRole(ACCEPT_ROLE) {
+        Request storage request = requests[requestId];
+        if (request.deadline < block.timestamp) {
+            revert DeadlineExpired();
         }
-        requests[id] = Request({id: id, price: _price, amount: _amount, deadline: _deadline, account: _account});
-        emit RequestTrade(_account, id, _price, _amount, _deadline);
-    }
-
-    function cancelTrade(uint128 _requestId) public {
-        Request storage request = requests[_requestId];
-        require(request.account == msg.sender, "Not owner");
-        requestIds.remove(_requestId);
-        delete requests[_requestId];
-        emit CancelTrade(msg.sender, _requestId);
-    }
-
-    function acceptTrade(uint128 _requestId, uint32 _price) public onlyOwner {
-        Request storage request = requests[_requestId];
-        require(request.deadline > block.timestamp, "Request expired");
 
         int256 baseAmount = request.amount;
-        int256 quoteAmount = floorDiv(-baseAmount * int256(uint256(_price)), int256(10 ** decimals));
+        int256 quoteAmount = computeQuoteAmount(price, baseAmount);
 
         if (baseAmount > 0) {
-            require(_price <= request.price, "Price too high");
-            base.transferFrom(vault, request.account, uint256(baseAmount));
+            if (price > request.price) {
+                revert PriceTooHigh();
+            }
+            if (!limiter.addOperation(uint256(baseAmount))) {
+                revert LimitExceeded();
+            }
+            IERC20(BASE_TOKEN).transferFrom(VAULT, request.account, uint256(baseAmount));
         } else {
-            require(_price >= request.price, "Price too low");
-            base.transferFrom(request.account, vault, uint256(-baseAmount));
+            if (price < request.price) {
+                revert PriceTooLow();
+            }
+            if (!limiter.addOperation(uint256(-baseAmount))) {
+                revert LimitExceeded();
+            }
+            IERC20(BASE_TOKEN).transferFrom(request.account, VAULT, uint256(-baseAmount));
         }
 
         if (quoteAmount > 0) {
-            quote.transferFrom(vault, request.account, uint256(quoteAmount));
+            IERC20(QUOTE_TOKEN).transferFrom(VAULT, request.account, uint256(quoteAmount));
         } else {
-            quote.transferFrom(request.account, vault, uint256(-quoteAmount));
+            IERC20(QUOTE_TOKEN).transferFrom(request.account, VAULT, uint256(-quoteAmount));
         }
 
-        requestIds.remove(_requestId);
-        delete requests[_requestId];
+        requestIds.remove(requestId);
+        delete requests[requestId];
 
-        emit AcceptTrade(request.account, _requestId, _price);
+        emit AcceptTrade(request.account, requestId, price);
+    }
+
+    function cancelRequest(uint128 requestId) public {
+        Request storage request = requests[requestId];
+        if (request.id != 0) {
+            if (request.account != msg.sender) {
+                revert SenderUnauthorized();
+            }
+            _deleteRequest(requestId);
+        }
+    }
+
+    function computeQuoteAmount(uint32 price, int256 baseAmount) public pure returns (int256) {
+        return floorDiv(-baseAmount * int256(uint256(price)), int256(10 ** PRICE_DECIMALS));
+    }
+
+    function deleteExpiredRequests() public {
+        uint128 id = requestIds.first();
+        while (id != 0) {
+            Request memory request = requests[id];
+            if (request.deadline < block.timestamp) {
+                _deleteRequest(id);
+            }
+            id = requestIds.next(id);
+        }
+    }
+
+    function deleteExpiredRequests(address account) public {
+        uint128 id = requestIds.first();
+        while (id != 0) {
+            Request memory request = requests[id];
+            if (request.deadline < block.timestamp && request.account == account) {
+                _deleteRequest(id);
+            }
+            id = requestIds.next(id);
+        }
     }
 
     function getRequests() public view returns (Request[] memory) {
@@ -107,18 +182,71 @@ contract Exchange is Ownable {
         return _requests;
     }
 
-    /**
-     * @dev Emitted when a trade is requested.
-     */
-    event RequestTrade(address indexed account, uint128 indexed id, uint32 price, int64 amount, uint32 deadline);
+    function requestTrade(uint32 price, int64 amount, uint32 deadline) public {
+        if (deadline < block.timestamp) {
+            revert DeadlineExpired();
+        }
+        address account = msg.sender;
+        deleteExpiredRequests(account);
+        uint128 id = requestIds.generate();
+        if (amount > 0) {
+            if (uint64(amount) < MINIMUM_AMOUNT) {
+                revert AmountTooLow();
+            }
+        } else {
+            if (uint64(-amount) < MINIMUM_AMOUNT) {
+                revert AmountTooLow();
+            }
+        }
+        requests[id] = Request({id: id, price: price, amount: amount, deadline: deadline, account: account});
+        emit RequestTrade(account, id, price, amount, deadline);
+    }
+
+    function requestTradeWithPermit(
+        uint32 price,
+        int64 amount,
+        uint32 deadline,
+        uint256 value,
+        uint8 v,
+        bytes32 r,
+        bytes32 s
+    ) public {
+        address account = msg.sender;
+        if (amount > 0) {
+            IERC20Permit(QUOTE_TOKEN).permit(account, address(this), value, deadline, v, r, s);
+        } else {
+            IERC20Permit(BASE_TOKEN).permit(account, address(this), value, deadline, v, r, s);
+        }
+        requestTrade(price, amount, deadline);
+    }
 
     /**
-     * @dev Emitted when a trade is cancelled.
+     * @dev Sets the 24-hour transfer limit.
+     * Can only be called by an account with the LIMIT_ROLE.
+     * @param _limit The limit value to be set.
      */
-    event CancelTrade(address indexed account, uint128 indexed id);
+    function setLimit(uint256 _limit) public onlyRole(LIMIT_ROLE) {
+        limiter.limit = _limit;
+        emit ChangeLimit(_limit);
+    }
 
     /**
-     * @dev Emitted when a trade is accepted.
+     * @dev Temporarily increases the 24-hour transfer limiter.
+     * Can only be called by an account with the LIMIT_ROLE.
+     * @param _limitIncrease Amount by which the limit should be increased.
      */
-    event AcceptTrade(address indexed account, uint128 indexed id, uint32 price);
+    function temporarilyIncreaseLimit(uint256 _limitIncrease) public onlyRole(LIMIT_ROLE) {
+        limiter.temporarilyIncreaseLimit(_limitIncrease);
+        emit TemporarilyIncreaseLimit(_limitIncrease);
+    }
+
+    /**
+     * @dev Temporarily decreases the 24-hour transfer limiter.
+     * Can only be called by an account with the LIMIT_ROLE.
+     * @param _limitDecrease Amount by which the limit should be decreased.
+     */
+    function temporarilyDecreaseLimit(uint256 _limitDecrease) public onlyRole(LIMIT_ROLE) {
+        limiter.temporarilyDecreaseLimit(_limitDecrease);
+        emit TemporarilyDecreaseLimit(_limitDecrease);
+    }
 }
---
+++ Limiter.sol
@@ -0,0 +1,84 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import {LinkedListLibrary, LinkedList} from "./LinkedList.sol";
+
+using LinkedListLibrary for LinkedList;
+
+struct Operation {
+    int256 amount;
+    uint256 timestamp;
+}
+
+struct Limiter {
+    uint256 interval;
+    uint256 limit;
+    LinkedList _keys;
+    mapping(uint128 => Operation) _operations;
+}
+
+using LimiterLibrary for Limiter;
+
+library LimiterLibrary {
+    function operations(Limiter storage self) internal view returns (Operation[] memory) {
+        Operation[] memory _operations = new Operation[](self._keys.length());
+        uint256 index = 0;
+        uint128 key = self._keys.first();
+        while (key != 0) {
+            _operations[index] = self._operations[key];
+            key = self._keys.next(key);
+            index++;
+        }
+        return _operations;
+    }
+
+    function temporarilyIncreaseLimit(Limiter storage self, uint256 _limitIncrease) internal {
+        _addUncheckedOperation(self, -int256(_limitIncrease));
+    }
+
+    function temporarilyDecreaseLimit(Limiter storage self, uint256 _limitDecrease) internal {
+        _addUncheckedOperation(self, int256(_limitDecrease));
+    }
+
+    function remainingLimit(Limiter storage self) internal view returns (int256) {
+        return int256(self.limit) - self.usedLimit();
+    }
+
+    function usedLimit(Limiter storage self) internal view returns (int256) {
+        int256 _sum = 0;
+        uint128 key = self._keys.first();
+        while (key != 0) {
+            if (self._operations[key].timestamp > block.timestamp - self.interval) {
+                _sum += self._operations[key].amount;
+            }
+            key = self._keys.next(key);
+        }
+        return _sum;
+    }
+
+    function _filterOperations(Limiter storage self) private {
+        uint128 key = self._keys.first();
+        while (key != 0) {
+            if (self._operations[key].timestamp > block.timestamp - self.interval) {
+                break;
+            }
+            delete self._operations[key];
+            key = self._keys.remove(key);
+        }
+    }
+
+    function _addOperationNode(Limiter storage self, int256 _amount) private {
+        uint128 key = self._keys.generate();
+        self._operations[key] = Operation({amount: int256(_amount), timestamp: block.timestamp});
+    }
+
+    function _addUncheckedOperation(Limiter storage self, int256 _amount) private {
+        _filterOperations(self);
+        _addOperationNode(self, _amount);
+    }
+
+    function addOperation(Limiter storage self, uint256 _amount) internal returns (bool) {
+        _addUncheckedOperation(self, int256(_amount));
+        return self.remainingLimit() >= 0;
+    }
+}
